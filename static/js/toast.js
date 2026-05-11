a4527f3af2f14a97c16eb2ce579f5074b68a45fe
IbnuSabilGitHub <ibnusabil2301@gmail.com>
2026-05-11 15:09:29 +0800

    fix: update template autentikasi dan halaman utama

--- static/js/toast.js
+++ static/js/toast.js
@@ -8,37 +8,35 @@ class ToastManager {
 
     static typeConfig = {
         success: {
-            iconClass: 'text-green-500',
+            iconClass: 'text-success',
+            iconName: 'tabler:check',
             borderClass: 'border-green-500/30',
             bgClass: 'bg-green-500/10',
         },
         error: {
-            iconClass: 'text-red-500',
+            iconClass: 'text-error',
+            iconName: 'tabler:alert-triangle',
             borderClass: 'border-red-500/30',
             bgClass: 'bg-red-500/10',
         },
         warning: {
-            iconClass: 'text-yellow-500',
+            iconClass: 'text-warning',
+            iconName: 'tabler:alert-circle',
             borderClass: 'border-yellow-500/30',
             bgClass: 'bg-yellow-500/10',
         },
         info: {
-            iconClass: 'text-blue-500',
+            iconClass: 'text-info',
+            iconName: 'tabler:info-circle',
             borderClass: 'border-blue-500/30',
             bgClass: 'bg-blue-500/10',
         },
         delete: {
-            iconClass: 'text-red-500',
+            iconClass: 'text-error',
+            iconName: 'tabler:trash',
             borderClass: 'border-red-500/30',
             bgClass: 'bg-red-500/10',
         },
-    };
-
-    static iconPaths = {
-        success: 'M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z',
-        error: 'M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z',
-        warning: 'M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z',
-        info: 'M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z',
     };
 
     /**
@@ -65,7 +63,7 @@ class ToastManager {
         // Set colors
         toastElement.classList.add(config.borderClass, config.bgClass);
         iconElement.classList.add(config.iconClass);
-        iconElement.innerHTML = `<path fill-rule="evenodd" d="${this.iconPaths[type] || this.iconPaths.info}" clip-rule="evenodd" />`;
+        iconElement.setAttribute('icon', config.iconName || this.typeConfig.info.iconName);
 
         messageElement.textContent = message;
 
@@ -127,7 +125,7 @@ class ToastManager {
         // Set colors
         toastElement.classList.add(config.borderClass, config.bgClass);
         iconElement.classList.add(config.iconClass);
-        iconElement.innerHTML = `<path fill-rule="evenodd" d="${this.iconPaths[type] || this.iconPaths.info}" clip-rule="evenodd" />`;
+        iconElement.setAttribute('icon', config.iconName || this.typeConfig.info.iconName);
 
         titleElement.textContent = title;
         messageElement.textContent = message;
